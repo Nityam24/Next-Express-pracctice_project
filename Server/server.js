bbb18064a20dff847937df3ab9080d869f78172a
Nityam24 <nityampatel2402@gmail.com>
2023-10-01 18:00:59 +0530

    fix: cors issues are fixed

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+app.use(cors());
 
 app.get("/api/home", (req, res) => {
   res.json({ message: "Welcome to the home page!" });
