4c5dbd23a5ff471621329b754338ce78e54c1f70
Nityam24 <nityampatel2402@gmail.com>
2023-10-01 17:38:22 +0530

    update: setup express and port

---
+++ Server/server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/api/home", (req, res) => {
+  res.json({ message: "Welcome to the home page!" });
+});
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
