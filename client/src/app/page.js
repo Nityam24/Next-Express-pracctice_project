220ab49e869abead94de62ef764793b38a31f126
Nityam24 <nityampatel2402@gmail.com>
2023-10-03 23:29:49 +0530

    new update

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,28 +1,19 @@
-"use client";
-
-import React, { useEffect } from "react";
-import { useState } from "react";
-
-export default function Home() {
-  const [message, setMessage] = useState("Loading...");
-
-  useEffect(() => {
-    res = fetch("https://localhost:8080/api/home")
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log(data);
-        setMessage(data.message);
-      })
-      .catch((error) => {
-        console.error("Fetch error:", error);
-        setMessage("Error fetching data.");
-      });
-  }, []);
+export default async function Home() {
+  res = await fetch("https://localhost:8080/api/home")
+    .then((res) => {
+      if (!res.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return res.json();
+    })
+    .then((data) => {
+      console.log(data);
+      setMessage(data.message);
+    })
+    .catch((error) => {
+      console.error("Fetch error:", error);
+      setMessage("Error fetching data.");
+    });
 
   return <div>{message}</div>;
 }
