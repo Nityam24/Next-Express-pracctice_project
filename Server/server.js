e1d76f3a45c152ba64ad137e068915f36db8d305
Nityam24 <nityampatel2402@gmail.com>
2023-10-03 23:13:56 +0530

    new update

--- Server/server.js
+++ Server/server.js
@@ -5,7 +5,7 @@ const PORT = 8080;
 app.use(cors());
 
 app.get("/api/home", (req, res) => {
-  res.json({ message: "Welcome to the home page!" });
+  res.send(json({ message: "Welcome to the home page!" }));
 });
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -7,11 +7,20 @@ export default function Home() {
   const [message, setMessage] = useState("Loading...");
 
   useEffect(() => {
-    fetch("https://localhost:8080/api/home")
-      .then((res) => res.json())
+    res = fetch("https://localhost:8080/api/home")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
       .then((data) => {
         console.log(data);
         setMessage(data.message);
+      })
+      .catch((error) => {
+        console.error("Fetch error:", error);
+        setMessage("Error fetching data.");
       });
   }, []);
 
